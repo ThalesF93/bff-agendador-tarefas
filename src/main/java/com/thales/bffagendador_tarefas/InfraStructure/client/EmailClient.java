772ab97f842330037b34786393c95daa81276071
thales <thalesgarcezf@gmail.com>
2025-07-04 15:30:05 -0300

    adicionando metodos de notificacao

---
+++ src/main/java/com/thales/bffagendador_tarefas/InfraStructure/client/EmailClient.java
@@ -0,0 +1,19 @@
+package com.thales.bffagendador_tarefas.InfraStructure.client;
+
+
+
+import com.thales.bffagendador_tarefas.Business.dto.TarefasDTO;
+import com.thales.bffagendador_tarefas.InfraStructure.Enuns.StatusNotificacaoEnum;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTO dto);
+}
